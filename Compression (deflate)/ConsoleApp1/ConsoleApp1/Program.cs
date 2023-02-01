d11abff1b89bad6ad51d780682c3746b11706318
hediye jamili <61234450+hedieh-hj@users.noreply.github.com>
2023-02-01 22:04:43 +1000

    update compressed algorithm
    there are some different ways to user deflatestream and gzipstream to compressed files.

--- Compression (deflate)/ConsoleApp1/ConsoleApp1/Program.cs
+++ Compression (deflate)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,90 +1,150 @@
-﻿using ConsoleApp1;
+using ConsoleApp1;
 using Microsoft.VisualBasic;
 using System.IO.Compression;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-
-StringBuilder data = new StringBuilder();
-
-for (int i = 1; i < 10001; i++)
+public class Program 
 {
-    data.AppendLine(string.Format("{0}\tCompress", i));
-}
+    private readonly compression _compressionclass;
 
-try
-{
-    #region 'make uncompress'
-    //uncompress----------------------------------
-    FileStream stream = new FileStream(@"C:\Users\orash\Desktop\compress.txt", FileMode.Create, FileAccess.Write);
-    StreamWriter writer = new StreamWriter(stream);
+    public Program(compression compression)
+    {
+        _compressionclass = compression;
+    }
+   
 
-    writer.Write(data.ToString());
-    writer.Close();
+    private static void Main(string[] args)
+    {
+        //made a new file
+        StringBuilder data = new StringBuilder();
 
-    Console.Write("test");
-    #endregion 'uncompress'
+        for (int i = 1; i < 10001; i++)
+        {
+            data.AppendLine(string.Format("{0}\tCompress", i));
+        }       
 
-    #region 'gzip'
-    //gzip--------------------------------------    
-    var stream_gzip = new FileStream(@"C:\Users\orash\Desktop\compressfilegzip.txt", FileMode.Create, FileAccess.Write);
-    var gzip = new GZipStream(stream_gzip, CompressionMode.Compress);
-    var writer_gzip = new StreamWriter(gzip);
+        try
+        {
+            #region 'make uncompress'
+            //uncompress----------------------------------
+            //FileStream stream = new FileStream(@"C:\Users\orash\Desktop\files_compression\compress.txt", FileMode.Create, FileAccess.Write);
+            //StreamWriter writer = new StreamWriter(stream);
 
-    writer_gzip.Write(data.ToString());
-    writer_gzip.Close();
+            //writer.Write(data.ToString());
+            //writer.Close();
 
-    Console.WriteLine("test gzip!");
-    #endregion 'gzip'
+            //Console.Write("test");
+            #endregion 'uncompress'
 
-    #region 'deflate'
-    //deflate-----------------------------------------    
-    var stream_deflate = new FileStream(@"C:\Users\orash\Desktop\compressfiledeflate.txt", FileMode.Create, FileAccess.Write);
-    var deflate = new DeflateStream(stream_deflate, CompressionMode.Compress);
-    var write_deflate = new StreamWriter(deflate);
+            #region 'gzip'
+            ////Gzip--------------------------------------
+            //var stream_gzip = new FileStream(@"C:\Users\orash\Desktop\files_compression\compressfilegzip.txt", FileMode.Create, FileAccess.Write);
+            //var gzip = new GZipStream(stream_gzip, CompressionMode.Compress);
+            //var writer_gzip = new StreamWriter(gzip);
 
-    write_deflate.Write(data.ToString());
-    write_deflate.Close();
+            //writer_gzip.Write(data.ToString());
+            //writer_gzip.Close();
 
-    Console.Write("test deflate!");
-    #endregion
+            //Console.WriteLine("test gzip!");
+            #endregion 'gzip'
 
-    #region 'second deflate'
-    //second deflate -----------------------------------------
+            #region 'deflate'
+            //deflate-----------------------------------------
+            //StreamReader file_deflate = new StreamReader("C:/Users/orash/Desktop/files_compression/t.txt");
 
-    //StreamReader file_deflate = new StreamReader("C:/Users/orash/Desktop/compressfiledeflate.txt");
+            //var stream_deflate = new FileStream(@"C:\Users\orash\Desktop\files_compression\t2-t.txt", FileMode.Create, FileAccess.Write);
+            //var deflate = new DeflateStream(stream_deflate, CompressionLevel.NoCompression);
+            //var write_deflate = new StreamWriter(deflate);
 
-    //var stream_deflate = new FileStream(@"C:\Users\orash\Desktop\compressfiledeflatesecond.txt", FileMode.Create, FileAccess.Write);
-    //var deflate = new DeflateStream(stream_deflate, CompressionMode.Compress);
-    //var write_deflate = new StreamWriter(deflate);
+            //write_deflate.Write(data.ToString());
+            //write_deflate.Close();
 
-    //write_deflate.Write(file_deflate.ToString());
-    //write_deflate.Close();
+            //Console.Write("test deflate!");
+            #endregion
 
-    //Console.WriteLine("test deflate2!");
-    #endregion 'second deflate'
+            #region 'second deflate'
+            //second deflate -----------------------------------------
 
-    #region 'decompress first deflate'
-    //decompress second-deflate 1:0 -------------------------------
+            //StreamReader file_deflate = new StreamReader("C:/Users/orash/Desktop/compressfiledeflate.txt");
 
-    var stream_deflate_decomp = new FileStream(@"C:\Users\orash\Desktop\compressfiledeflate.txt", FileMode.Open, FileAccess.Read);
-    var deflate_decompress = new DeflateStream(stream_deflate_decomp, CompressionMode.Decompress);
-    var write_undeflate = new StreamReader(deflate_decompress);
+            //var stream_deflate = new FileStream(@"C:\Users\orash\Desktop\compressfiledeflatesecond.txt", FileMode.Create, FileAccess.Write);
+            //var deflate = new DeflateStream(stream_deflate, CompressionMode.Compress);
+            //var write_deflate = new StreamWriter(deflate);
 
-    string decomp_second = write_undeflate.ReadToEnd();
-    write_undeflate.Close();
+            //write_deflate.Write(file_deflate.ToString());
+            //write_deflate.Close();
 
-    //save decomp_second in new file
-    var fs = new FileStream(@"C:\Users\orash\Desktop\decomp1.txt", FileMode.Create);
-    var w = new StreamWriter(fs, Encoding.UTF8);
-    w.WriteLine(decomp_second);
+            //Console.WriteLine("test deflate2!");
+            #endregion 'second deflate'
 
-    Console.Write(decomp_second);
+            #region 'decompress first deflate'
+            //decompress second-deflate 1:0 -------------------------------
 
-    #endregion 'decompress first deflate'
+            //var file = new FileStream(@"C:\Users\orash\Desktop\files_compression\t2.txt", FileMode.Open, FileAccess.Read);
+
+            ////decompressed
+            //var deflate_decompress = new DeflateStream(file, CompressionMode.Decompress);
+
+            ////save result to new file
+            //FileStream outputFileStream = File.Create(@"C:\Users\orash\Desktop\files_compression\decomp-t2.txt");
+            //deflate_decompress.CopyTo(outputFileStream);
+            //Console.Write(outputFileStream);
+
+            #endregion 'decompress first deflate'
+
+            #region 'decompress 2 deflate'
+            //decompress second deflate to first deflate------------------
+            //var stream2 = new FileStream(@"C:\Users\orash\Desktop\files_compression\compressfiledeflatesecond.txt", FileMode.Open, FileAccess.Read);
+            //var deflate_stream2 = new DeflateStream(stream2, CompressionMode.Decompress);
+
+            /////save to new 
+            //FileStream outputFileStream = File.Create(@"C:\Users\orash\Desktop\files_compression\test2.txt");
+            //deflate_stream2.CopyTo(outputFileStream);
 
 
-}
-catch (IOException ex)
-{
-    Console.WriteLine("Error" + ex.Message);
+            //FileStream compressedFileStream = File.Open(CompressedFileName, FileMode.Open);
+            //FileStream outputFileStream = File.Create(@"C:\Users\orash\Desktop\files_compression\test.txt");
+            //var decompressor = new DeflateStream(compressedFileStream, CompressionMode.Decompress);
+            //decompressor.CopyTo(outputFileStream);
+            #endregion 'decompress 2 deflate'
+           
+            #region 'three level compressing'
+            // compress a file to gzip then covert it to deflate 
+            //1.
+            var stream_deflate = new FileStream(@"C:\Users\orash\Desktop\files_compression\1gzip.txt", FileMode.Create, FileAccess.Write);
+            var gzip = new GZipStream(stream_deflate, CompressionMode.Compress);
+            var write_gzip = new StreamWriter(gzip);
+
+            write_gzip.Write(data.ToString());
+            write_gzip.Close();
+            Console.Write("convert to gzip!");
+
+            //2.
+            var stream_deflate2 = new FileStream(@"C:\Users\orash\Desktop\files_compression\2deflate.txt", FileMode.Create, FileAccess.Write);
+            var deflate = new DeflateStream(stream_deflate2, CompressionMode.Compress);
+            var write_deflate = new StreamWriter(deflate);
+
+            write_deflate.Write(write_gzip.ToString());
+            write_deflate.Close();
+            Console.Write("\nconvert to deflate!");
+
+
+            //3. it didn't decrease
+            //StreamReader file_deflate = new StreamReader("C:/Users/orash/Desktop/2deflate.txt");
+            var stream_deflate4 = new FileStream(@"C:\Users\orash\Desktop\files_compression\31gzip.txt", FileMode.Create, FileAccess.Write);
+            var gzip4 = new GZipStream(stream_deflate4, CompressionMode.Compress);
+            var write_gzip4 = new StreamWriter(gzip4);
+
+            write_gzip4.Write(write_deflate.ToString());
+            write_gzip4.Close();
+            Console.Write("convert to gzip!");
+            #endregion 'three level compressing'
+
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Error" + ex.Message);
+        }
+    }
 }
